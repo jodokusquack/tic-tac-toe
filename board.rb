31fcf531b3d9a3b8295889a68655276649fbd26c
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-02-09 22:58:04 +0100

    Add files for classes

---
+++ board.rb
@@ -0,0 +1,7 @@
+#/!/usr/bin/env ruby
+
+class Board
+  def initialize()
+    puts "New Board initialized."
+  end
+end
---
+++ game.rb
@@ -0,0 +1,10 @@
+require_relative board
+
+class Game
+  def initialize()
+  end
+
+  def start()
+    board = Board.new
+  end
+end
