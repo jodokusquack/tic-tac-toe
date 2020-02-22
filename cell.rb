4c1af47b24686be6fca25dcd5839ee7cbbc4cdda
Jakob Blaschke <jakobblaschke@yahoo.de>
2020-02-22 15:13:59 +0100

    Add Player and Cell classes

---
+++ cell.rb
@@ -0,0 +1,29 @@
+# /!/usr/bin/env ruby
+
+class Cell
+
+  attr_accessor :content
+
+  def initialize(content)
+    @content = content
+  end
+
+  def draw()
+    print " #{@content} "
+  end
+
+  # define the equality operator, so that two cells are only equal if they
+  # are not empty and have the same sign
+  def ==(other)
+    if self.class == other.class
+      if @content == " "
+        return false
+      end
+      return @content == other.content
+    else
+      return false
+    end
+  end
+  
+
+end
---
+++ player.rb
@@ -0,0 +1,29 @@
+class Player
+
+  attr_reader :sign
+
+  def initialize(sign)
+    @sign = sign
+  end
+
+  def set_move()
+    begin
+      puts("Player #{@sign}, where do you want to make your move? row:col")
+      input = gets.chomp()
+      if input[0].capitalize == "Q"
+        abort("You quit the program") 
+      end
+      if input.length != 3
+        raise ArgumentError, "\e[31mPlease type in the format #:#.\n"\
+        "The first # symbolizes the row and the second # the column.\e[0m"
+      end
+
+      if input[0].to_i > 3 or input[0].to_i < 1 or input[2].to_i > 3 or input[2].to_i < 1
+        raise ArgumentError, "\e[31mPlease only input numbers from 1-3.\e[0m"
+      end
+    end
+    row = input[0].to_i - 1
+    col = input[2].to_i - 1
+    return row, col
+  end
+end
